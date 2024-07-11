225b336b9812e3e9d64999d53af4c11b61c90956
Rafaela <104620799+rafaeladev@users.noreply.github.com>
2024-07-11 14:33:59 +0200

    [test] introduction with GSAP

--- src/components/LoadingBar.jsx
+++ src/components/LoadingBar.jsx
@@ -1,35 +1,69 @@
-import React, { useState, useEffect } from 'react';
-import { gsap } from 'gsap';
-import '../styles/LoadingBar.css'; // Assurez-vous d'avoir un fichier CSS pour les styles
+import React, { useState, useEffect } from "react";
+import { gsap } from "gsap";
+import "../styles/LoadingBar.css"; // Assurez-vous d'avoir un fichier CSS pour les styles
+import { FadeLoader } from "react-spinners";
 
-function LoadingBar({ isLoading, progress }) {
-    const [isLoaded, setIsLoaded] = useState(false);
+function LoadingBar({ progress }) {
+  const [isLoaded, setIsLoaded] = useState(false);
 
-    useEffect(() => {
-        if (!isLoading && isLoaded) {
-            gsap.to('.loading-bar', {
-                scaleX: 0,
-                duration: 3,
-                delay: 1,
-                onComplete: () => {
-                    document.querySelector('.loading-bar').classList.add('ended');
-                },
-            });
-        }
-    }, [isLoading, isLoaded]);
+  useEffect(() => {
+    if (progress >= 1) {
+      setIsLoaded(true);
+    }
+  }, [progress]);
 
-    useEffect(() => {
-        if (progress >= 1) {
-            setIsLoaded(true);
-        }
-    }, [progress]);
+  useEffect(() => {
+    if (isLoaded) {
+      const tl = gsap.timeline();
 
-    return (
-        <div
-            className='loading-bar'
-            style={{ transform: `scaleX(${progress})` }}
-        ></div>
-    );
+      window.setTimeout(() => {
+        /* // Ajout de la classe 'ended' à la barre de chargement
+        gsap.to(".loading-bar", {
+          scaleX: 0,
+          transformOrigin: "right top",
+          duration: 0.5, // Vous pouvez ajuster la durée de l'animation
+          ease: "ease-in-out",
+        });
+        gsap.to(".loading-container", {
+          opacity: 0,
+          duration: 1, // Vous pouvez ajuster la durée de l'animation
+          onComplete: () => {
+            document.querySelector(".loading-container").classList.add("ended");
+          },
+        }); */
+
+        tl.to(".loading-bar", {
+          scaleX: 0,
+          transformOrigin: "right top",
+          duration: 1.5, // Ajustez la durée de l'animation
+          ease: "ease-in-out",
+          onComplete: () => {
+            document.querySelector(".loading-bar").classList.add("ended");
+          },
+        }).to(".loading-container", {
+          opacity: 0,
+          duration: 0.5, // Ajustez la durée de l'animation
+          onComplete: () => {
+            document.querySelector(".loading-container").classList.add("ended");
+            /*   document.querySelector(".loading-bar").classList.add("ended"); */
+          },
+        });
+      }, 1000);
+    }
+  }, [isLoaded]);
+
+  return (
+    <div className="loading-container">
+      <div
+        className="loading-bar"
+        style={{ transform: `scaleX(${progress})` }}
+      ></div>
+
+      {/* <div className="loading-spinner">
+        <FadeLoader color="#ffffff" />
+      </div> */}
+    </div>
+  );
 }
 
 export default LoadingBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,201 +1,183 @@
-import React, { useEffect, useState, Suspense } from 'react';
+import React, { useEffect, useState, Suspense } from "react";
 
-import { Canvas } from '@react-three/fiber';
-import { Leva } from 'leva';
-import { Sky } from '@react-three/drei';
-import { Environment } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
+import { Canvas } from "@react-three/fiber";
+import { Leva } from "leva";
+import { Sky } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
-import Interface from '../components/Interface.jsx';
-import Cuisine from '../components/Cuisine.jsx';
-import LoadingBar from '../components/LoadingBar.jsx';
+import Interface from "../components/Interface.jsx";
+import Cuisine from "../components/Cuisine.jsx";
+import LoadingBar from "../components/LoadingBar.jsx";
 
-import { Perf } from 'r3f-perf';
+import { Perf } from "r3f-perf";
 
-import { useControls } from 'leva';
-import Scene from '../Scene.jsx';
-import FadeLoaderComponent from '../components/FadeLoader.jsx';
+import { useControls } from "leva";
+import Scene from "../Scene.jsx";
+import FadeLoaderComponent from "../components/FadeLoader.jsx";
 
-import { Vector3 } from 'three';
+import { Vector3 } from "three";
 
 function Home() {
-    const [isScenarioChanged, setIsScenarioChanged] = useState(false);
-    const [isWaterMoving, setIsWaterMoving] = useState(false);
-    const [isWaterMovingUp, setIsWaterMovingUp] = useState(false);
-    const [isReset, setIsReset] = useState(false);
-    const [resetKey, setResetKey] = useState(0);
+  const [isScenarioChanged, setIsScenarioChanged] = useState(false);
+  const [isWaterMoving, setIsWaterMoving] = useState(false);
+  const [isWaterMovingUp, setIsWaterMovingUp] = useState(false);
+  const [isReset, setIsReset] = useState(false);
+  const [resetKey, setResetKey] = useState(0);
 
-    const [loading, setLoading] = useState(true);
+  // Loading bar
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [progress, setProgress] = useState(0);
 
-    // Camera animation
-    const [cameraPosition, setCameraPosition] = useState(new Vector3(-12.08, 5.28, 9.7));
-    const [cameraTarget, setCameraTarget] = useState(new Vector3(0, 0, 0));
-    const [moveCamera, setMoveCamera] = useState(true);
-    const [menuButtonClick, setMenuButtonClick] = useState(false);
+  // Camera animation
+  const [cameraPosition, setCameraPosition] = useState(
+    new Vector3(-12.08, 5.28, 9.7)
+  );
+  const [cameraTarget, setCameraTarget] = useState(new Vector3(0, 0, 0));
+  const [moveCamera, setMoveCamera] = useState(true);
+  const [menuButtonClick, setMenuButtonClick] = useState(false);
 
-    // Texture change
-    const [changeTextures, setChangeTextures] = useState(false);
-    const [resetTextures, setResetTextures] = useState(false);
+  // Texture change
+  const [changeTextures, setChangeTextures] = useState(false);
+  const [resetTextures, setResetTextures] = useState(false);
 
-    // Change of states for the water animation
-    const toggleWaterMovingUp = () => {
-        setIsWaterMovingUp((prev) => !prev);
-    };
+  // Change of states for the water animation
+  const toggleWaterMovingUp = () => {
+    setIsWaterMovingUp((prev) => !prev);
+  };
 
-    const toggleWaterMoving = () => {
-        setIsWaterMoving((prev) => !prev);
-    };
+  const toggleWaterMoving = () => {
+    setIsWaterMoving((prev) => !prev);
+  };
 
-    // Change of states for the scenario
-    const toggleScenario = () => {
-        setIsScenarioChanged((prev) => !prev); // Toggle entre true et false
-    };
+  // Change of states for the scenario
+  const toggleScenario = () => {
+    setIsScenarioChanged((prev) => !prev); // Toggle entre true et false
+  };
 
-    // Change of states for the reset
-    const toggleReset = () => {
-        setIsReset((prev) => !prev);
-    };
+  // Change of states for the reset
+  const toggleReset = () => {
+    setIsReset((prev) => !prev);
+  };
 
-    // Change of states for the textures
-    const toggleTextures = () => {
-        setChangeTextures((prev) => !prev);
-    };
+  // Change of states for the textures
+  const toggleTextures = () => {
+    setChangeTextures((prev) => !prev);
+  };
 
-    // Change of states for the camera position
-    const toggleCameraPosition = () => {
-        setMoveCamera((prev) => !prev);
-    };
+  // Change of states for the camera position
+  const toggleCameraPosition = () => {
+    setMoveCamera((prev) => !prev);
+  };
 
-    // Function do handle the change of positions for the camera
-    const handleCameraPosition = (location) => {
-        setMoveCamera(true); // Passez en mode automatique
-        setMenuButtonClick(true);
-        if (location === 'cuisine') {
-            setCameraPosition(new Vector3(5.94, 1.85, 4.13));
-            setCameraTarget(new Vector3(5, 1, 0));
-        } else if (location === 'salon') {
-            setCameraPosition(new Vector3(-0.88, 1, 1.25));
-            setCameraTarget(new Vector3(3, 0.34, -3.2));
-        } else if (location === 'sdb') {
-            setCameraPosition(new Vector3(3.65, 1.25, -5.42));
-            setCameraTarget(new Vector3(8, 0, 0));
-        } else if (location === 'jardin') {
-            setCameraPosition(new Vector3(-7.12, 3.64, 9.33));
-            setCameraTarget(new Vector3(3, 0.34, -3.2));
-        }
-    };
+  // Function do handle the change of positions for the camera
+  const handleCameraPosition = (location) => {
+    setMoveCamera(true); // Passez en mode automatique
+    setMenuButtonClick(true);
+    if (location === "cuisine") {
+      setCameraPosition(new Vector3(5.94, 1.85, 4.13));
+      setCameraTarget(new Vector3(5, 1, 0));
+    } else if (location === "salon") {
+      setCameraPosition(new Vector3(-0.88, 1, 1.25));
+      setCameraTarget(new Vector3(3, 0.34, -3.2));
+    } else if (location === "sdb") {
+      setCameraPosition(new Vector3(3.65, 1.25, -5.42));
+      setCameraTarget(new Vector3(8, 0, 0));
+    } else if (location === "jardin") {
+      setCameraPosition(new Vector3(-7.12, 3.64, 9.33));
+      setCameraTarget(new Vector3(3, 0.34, -3.2));
+    }
+  };
 
-    // Function to reset of the animations
-    const handleReset = () => {
-        setIsReset(true);
-        setIsScenarioChanged();
-        setIsWaterMoving();
-        setIsWaterMovingUp();
-        setResetKey((prevKey) => prevKey + 1); // Change the key to force a re-render
+  // Function to reset of the animations
+  const handleReset = () => {
+    setIsReset(true);
+    setIsScenarioChanged();
+    setIsWaterMoving();
+    setIsWaterMovingUp();
+    setResetKey((prevKey) => prevKey + 1); // Change the key to force a re-render
 
-        setChangeTextures(false);
-        setResetTextures(true);
+    setChangeTextures(false);
+    setResetTextures(true);
 
-        setTimeout(() => {
-            setIsReset(false);
-            setResetTextures(false);
-        }, 200); // Reset the reset state after a short delay to allow re-rendering
-    };
+    setTimeout(() => {
+      setIsReset(false);
+      setResetTextures(false);
+    }, 200); // Reset the reset state after a short delay to allow re-rendering
+  };
 
-    // UseEffect to handle loading states
-    useEffect(() => {
-        const handleLoadingComplete = () => {
-            setLoading(false); // Met à jour l'état de chargement
-        };
-
-        // Simulez un chargement de 3 secondes pour la démo
-        const timeoutId = setTimeout(handleLoadingComplete, 3000);
-
-        return () => clearTimeout(timeoutId);
-    }, []);
-
-    const { dLightPosition, dLightIntensity } = useControls('Directional Light', {
-        dLightPosition: { value: [0, 10, 0], step: 0.1 },
-        dLightIntensity: { value: 1, step: 0.1 },
+  const { envMapIntensity, envMapHeight, envMapRadius, envMapScale } =
+    useControls("environment map", {
+      envMapIntensity: { value: 7, min: 0, max: 12 },
+      envMapHeight: { value: 7, min: 0, max: 100 },
+      envMapRadius: { value: 28, min: 10, max: 1000 },
+      envMapScale: { value: 100, min: 10, max: 1000 },
     });
 
-    const { envMapIntensity, envMapHeight, envMapRadius, envMapScale } = useControls(
-        'environment map',
-        {
-            envMapIntensity: { value: 7, min: 0, max: 12 },
-            envMapHeight: { value: 7, min: 0, max: 100 },
-            envMapRadius: { value: 28, min: 10, max: 1000 },
-            envMapScale: { value: 100, min: 10, max: 1000 },
-        }
-    );
+  useEffect(() => {
+    console.log(progress);
+  }, [isLoaded, progress]);
 
-    return (
-        <>
-            {/* <LoadingBar isLoading={isLoading} progress={progress} /> */}
+  return (
+    <>
+      <LoadingBar isLoading={!isLoaded} progress={progress} />
 
-            <Leva
-                hidden={false}
-                collapsed={true}
+      <Leva hidden={false} collapsed={true} />
+
+      {/* <Suspense fallback={<FadeLoaderComponent />}> */}
+      <>
+        <div className="webgl">
+          <Canvas
+            shadows={true}
+            flat
+            camera={{
+              fov: 45,
+              near: 0.1,
+              far: 200,
+              position: cameraPosition,
+            }}
+          >
+            <color attach="background" args={["#302D38"]} />
+            <Perf position="top-left" />
+            <Environment preset="sunset" intensity={envMapIntensity} />
+
+            <Scene
+              isScenarioChanged={isScenarioChanged}
+              toggleScenario={toggleScenario}
+              toggleAnimation={toggleWaterMoving}
+              toggleWaterMovingUp={toggleWaterMovingUp}
+              isWaterMoving={isWaterMoving}
+              isWaterMovingUp={isWaterMovingUp}
+              toggleReset={toggleReset}
+              isReset={isReset}
+              resetTextures={resetTextures}
+              changeTextures={changeTextures}
+              toggleTextures={toggleTextures}
+              cameraPosition={cameraPosition}
+              cameraTarget={cameraTarget}
+              moveCamera={moveCamera}
+              setMoveCamera={toggleCameraPosition}
+              menuButtonClick={menuButtonClick}
+              setIsLoaded={setIsLoaded}
+              setLoadingProgress={setProgress}
             />
-
-            {/* <Suspense fallback={<FadeLoaderComponent />}> */}
-            <>
-                <div className='webgl'>
-                    <Canvas
-                        shadows={true}
-                        flat
-                        camera={{
-                            fov: 45,
-                            near: 0.1,
-                            far: 200,
-                            position: cameraPosition,
-                        }}
-                    >
-                        <color
-                            attach='background'
-                            args={['#302D38']}
-                        />
-                        <Perf position='top-left' />
-                        <Environment
-                            preset='sunset'
-                            intensity={envMapIntensity}
-                        />
-
-                        <Scene
-                            isScenarioChanged={isScenarioChanged}
-                            toggleScenario={toggleScenario}
-                            toggleAnimation={toggleWaterMoving}
-                            toggleWaterMovingUp={toggleWaterMovingUp}
-                            isWaterMoving={isWaterMoving}
-                            isWaterMovingUp={isWaterMovingUp}
-                            toggleReset={toggleReset}
-                            isReset={isReset}
-                            resetTextures={resetTextures}
-                            changeTextures={changeTextures}
-                            toggleTextures={toggleTextures}
-                            cameraPosition={cameraPosition}
-                            cameraTarget={cameraTarget}
-                            moveCamera={moveCamera}
-                            setMoveCamera={toggleCameraPosition}
-                            menuButtonClick={menuButtonClick}
-                        />
-                    </Canvas>
-                </div>
-                <Interface
-                    toggleWaterMovingUp={toggleWaterMovingUp}
-                    toggleWaterMoving={toggleWaterMoving}
-                    isWaterMovingUp={isWaterMovingUp}
-                    isWaterMoving={isWaterMoving}
-                    isScenarioChanged={isScenarioChanged}
-                    handleReset={handleReset}
-                    isReset={isReset}
-                    toggleReset={toggleReset}
-                    handleCameraPosition={handleCameraPosition}
-                />
-            </>
-            {/* </Suspense> */}
-            {/* )} */}
-        </>
-    );
+          </Canvas>
+        </div>
+        <Interface
+          toggleWaterMovingUp={toggleWaterMovingUp}
+          toggleWaterMoving={toggleWaterMoving}
+          isWaterMovingUp={isWaterMovingUp}
+          isWaterMoving={isWaterMoving}
+          isScenarioChanged={isScenarioChanged}
+          handleReset={handleReset}
+          isReset={isReset}
+          toggleReset={toggleReset}
+          handleCameraPosition={handleCameraPosition}
+        />
+      </>
+      {/* </Suspense> */}
+      {/* )} */}
+    </>
+  );
 }
 export default Home;
